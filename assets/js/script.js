d592004b4b06e2f15b1858fa06ce1d5926728d84
B0rtleinacan <Cloverlyn123@gmail.com>
2023-04-01 17:12:30 -0500

    Declared html elements in js file to created the interactive quiz portion.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var body = document.body;
+var h1El = document.createElement('h1');
+var sectionEl = document.createElement('section');
+var divEl = document.createElement('div');
+var btn = document.createElement('button');
