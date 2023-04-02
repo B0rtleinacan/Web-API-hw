8c8eb1dd84a965c9f3055235f3f2405da8d86e41
B0rtleinacan <Cloverlyn123@gmail.com>
2023-04-02 18:08:36 -0500

    added code to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
-var body = document.body;
-var h1El = document.createElement('h1');
-var sectionEl = document.createElement('section');
-var divEl = document.createElement('div');
-var btn = document.createElement('button');
+var startButton = document.getElementById('start-btn');
+var questionContainerElement = document.getElementById('question-container');
 
-var timer;
-var timerCounter;
+var shuffledQuestions, currentQuestionsIndex;
+
+startButton.addEventListener('click', startGame);
+
+function startGame() {
+    console.log('started');
+    startButton.classList.add('hide');
+    shuffledQuestions = question.sort(() => Math.random() -.5);
+    currentQuestionsIndex = 0;
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionsIndex])
+}
+
+function showQuestion(question) {
+
+}
+
+function selectAnswer() {}
+
+var question = [
+    {
+        question: 'What is the coolest Fortnite skin?',
+        answer: [
+            { text: 'Jogn Wick', correct: true },
+            { text: 'P.A.N.D.A Team Leader', correct: false }
+        ]
+
+    }
+]
